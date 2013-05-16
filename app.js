9786d08f73af134d7786abb4058fde50ccd298a1
Quang <herokes@yahoo.com>
2013-05-16 11:24:23 +0700

    site with express

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-var http = require("http");
-var a = '';
-	http.createServer(function(request, response){
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write("Hello world");
-		response.end();
-}).listen(3000);
-debugger;
-a = 'www';
-console.log("Server has started.");
+/*
+ * Module dependencies
+ */
+var express = require('express'),
+	stylus = require('stylus'),
+	nib = require('nib')
+var app = express()
+function compile(str, path) {
+	return stylus(str)
+		.set('filename', path)
+		.use(nib())
+}
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.logger('dev'))
+app.use(stylus.middleware(
+	{	src: __dirname + '/public',
+		compile: compile
+	}
+))
+app.use(express.static(__dirname + '/public'))
+
+//app.get('/', function (req, res) {
+//  res.end('Hi there!')
+//})
+app.get('/', function (req, res) {
+  res.render('index',
+  { title : 'Home' }
+  )
+  console.log("ok la");
+})
+app.listen(3000)
+
