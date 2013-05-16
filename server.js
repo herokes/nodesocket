f3b7bd4c57cc00f478561bd311d932ba9654eb22
Quang <herokes@yahoo.com>
2013-05-16 13:41:51 +0700

    fix socket server

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var io = require('socket.io');
 var port = process.env.PORT || 5000;
 
+// assuming io is the Socket.IO server object
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+
 var socket = io.listen(port, function() {
   console.log("Listening on " + port);
 });
